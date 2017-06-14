4148dcde8fcc630a0093b4b12bc0657ad3bcda8e
Eduardo Santos Marques <eduardo.marques@avixy.com>
2017-06-13 21:36:30 -0300

    Initial commit

---
+++ crossover_requisite_analysis.mm
@@ -0,0 +1,20 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1497306700876" ID="ID_647776717" MODIFIED="1497306706647" TEXT="crossover assignment">
+<node CREATED="1497306707235" ID="ID_1135276494" MODIFIED="1497306709581" POSITION="right" TEXT="Users">
+<node CREATED="1497306709820" ID="ID_385021372" MODIFIED="1497306734340" TEXT="Customers">
+<node CREATED="1497306734654" ID="ID_1653271082" MODIFIED="1497306739893" TEXT="Can place support requests"/>
+<node CREATED="1497306808653" ID="ID_959788412" MODIFIED="1497306823889" TEXT="view satatus of previous requests"/>
+</node>
+<node CREATED="1497306741516" ID="ID_529293168" MODIFIED="1497306745921" TEXT="Support agents">
+<node CREATED="1497306752991" ID="ID_799207966" MODIFIED="1497306849691" TEXT="find and process support requests"/>
+<node CREATED="1497306850636" ID="ID_383907761" MODIFIED="1497306876481" TEXT="report with request data from the last month"/>
+<node CREATED="1497306876830" ID="ID_1028780586" MODIFIED="1497306890358" TEXT="this report should be PDF exportable"/>
+</node>
+<node CREATED="1497306908618" ID="ID_152153911" MODIFIED="1497306909684" TEXT="Admin">
+<node CREATED="1497306941745" ID="ID_1485940189" MODIFIED="1497306946764" TEXT="Manage users"/>
+<node CREATED="1497306947115" ID="ID_262947008" MODIFIED="1497306951870" TEXT="Manage system objects"/>
+</node>
+</node>
+</node>
+</map>
---
+++ support-request-system/README.md
@@ -0,0 +1,36 @@
+# Ticket Support System
+
+The Ticker Support System is an application created to help Customers and Support agents to create, track and act upon suport requests (from customers). Agents will use this system to control the support tickets and give customers feedback about how their support requests are being handled.
+
+## Basic usage
+
+Configure the environment (development). The development environment is configured to use SQLite as database:
+
+```shell
+bundle install
+```
+
+And run with:
+
+```shell
+bin/rails server
+```
+
+View at: [http://localhost:3000](http://localhost:3000)
+
+## Table of Contents
+
+* [Ticket Support System](#ticket-support-system)
+* [Table of Contents](#table-of-contents)
+* [Ruby version](#ruby-version)
+* [System dependencies](#system-dependencies)
+* [Configuration](#configuration)
+* [Database creation](#database-creation)
+* [Database initialization](#database-initialization)
+* [How to run the test suite](how-to-run-the-test-suite)
+<!--* Services (job queues, cache servers, search engines, etc.)-->
+* [Deployment instructions](#deployment-instructions)
+
+## Ruby version
+
+The Ruby version used in this application is 2.4.0 MRI
